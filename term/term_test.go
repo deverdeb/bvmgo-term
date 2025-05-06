b38c363623536b83f05d346c2e2e57323393627d
deverdeb <ben2verdelhan@gmail.com>
2025-05-06 22:04:16 +0200

    fix(term): Fix tests

--- term/term_test.go
+++ term/term_test.go
@@ -1,18 +1,31 @@
 package term
 
+import "os"
+
+func ExamplePrintln() {
+	output = os.Stdout
+	Println("hello world")
+	// Output:
+	// hello world
+	//
+}
+
 func ExamplePrint() {
+	output = os.Stdout
 	Print("hello world")
 	// Output:
 	// hello world
 }
 
 func ExamplePrintf() {
+	output = os.Stdout
 	Printf("hello %s", "world")
 	// Output:
 	// hello world
 }
 
 func ExampleCursorMoveTo() {
+	output = os.Stdout
 	CursorMoveTo(2, 3)
 	Print("hello world")
 	// Output:
